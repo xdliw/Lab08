0b6a8b4546e6e86bb64ee6cccd8c9dfa67dc9de9
xdliw <90445100+xdliw@users.noreply.github.com>
2023-12-25 01:52:09 +0300

    Initial commit

---
+++ ClassLibrary/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int TechnicalInspectionYear { get; set; }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using ClassLibrary;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        List<Car> cars = new List<Car>();
+
+        var rnd = new Random();
+        for (int i = 0; i < 1000; i++)
+        {
+            Car car = new Car();
+            car.Id = i;
+            car.Name = $"Car {i}";
+            car.TechnicalInspectionYear = 2020 + rnd.Next(10);
+            cars.Add(car);
+        }
+
+        StreamService<Car> streamService = new StreamService<Car>();
+
+        MemoryStream memoryStream = new MemoryStream();
+
+        streamService.WriteToStreamAsync(memoryStream, cars);
+        Console.WriteLine("hey1");
+        Thread.Sleep(100);
+        streamService.CopyFromStreamAsync(memoryStream, "file.txt");
+        Console.WriteLine("hey2");
+        Console.WriteLine(await streamService.GetStatisticsAsync("file.txt", (Car car) => car.TechnicalInspectionYear == 2022));
+    }
+}
+
+
